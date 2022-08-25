c576e7f23d83e5cd5704e46e8b62576da1473ad1
Bibin Jose <bibinmjose@gmail.com>
2022-08-26 00:19:59 +1000

    train script working on HPO.Should be refatored for params

--- train.py
+++ train.py
@@ -15,8 +15,6 @@ from sklearn.metrics import r2_score
 from sklearn.model_selection import cross_val_score
 
 
-
-
 def load_files(args):
     '''Load all datasets into a datadict
     '''
@@ -30,7 +28,7 @@ def load_files(args):
 
     return _dict
 
-def get_embedded_feat(feat, encod_dict, feat_name='Family_Code'):
+def get_embedded_feat(feat, encod_dict, feat_name=None):
     '''Return embeddings for each feature column using encoded dict
     '''
     feat_df = feat.to_frame()
@@ -54,39 +52,43 @@ def impute_missing(df, out_dir):
     '''Returns imputed tesonr for train
     Saves the imputer to out_path.
     '''
-    knn = KNNImputer()
-    knn.fit(df)
-    df_imputed = knn.transform(df)
-    dump(knn, os.path.join(out_dir,'knnimputer.joblib'))
+    # knn = KNNImputer()
+    # knn.fit(df)
+    # df_imputed = knn.transform(df)
+    # dump(knn, os.path.join(out_dir,'knnimputer.joblib'))
+    # np.save(os.path.join(out_dir,'X_train.npy'), df_imputed) # save
+    df_imputed = np.load(os.path.join(out_dir,'X_train.npy'))
     return df_imputed
 
-def objective(trial, X_train, y_train):
-    '''Objective function for the optimizer
+def make_objective(X_train, y_train):
+    '''Create and returns Objective function for the optimizer
     '''
-    param = {
-        "silent": 1,
-        "objective": "reg:squarederror",
-        "eval_metric": r2_score,
-        "booster": trial.suggest_categorical("booster", ["gbtree", "gblinear", "dart"]),
-        "lambda": trial.suggest_loguniform("lambda", 1e-8, 1.0e6),
-        "alpha": trial.suggest_loguniform("alpha", 1e-8, 1.0e6),
-    }
-    if param["booster"] == "gbtree" or param["booster"] == "dart":
-        param["max_depth"] = trial.suggest_int("max_depth", 2, 5)
-        param["eta"] = trial.suggest_loguniform("eta", 1e-8, 0.05)
-        param["gamma"] = trial.suggest_loguniform("gamma", 1e-8, 1.0)
-        param["grow_policy"] = trial.suggest_categorical("grow_policy", ["depthwise", "lossguide"])
-    if param["booster"] == "dart":
-        param["sample_type"] = trial.suggest_categorical("sample_type", ["uniform", "weighted"])
-        param["normalize_type"] = trial.suggest_categorical("normalize_type", ["tree", "forest"])
-        param["rate_drop"] = trial.suggest_loguniform("rate_drop", 1e-8, 1.0)
-        param["skip_drop"] = trial.suggest_loguniform("skip_drop", 1e-8, 1.0)
-    
-    model = xgb.XGBRegressor(**param)
-    
-    return cross_val_score(model, X_train, y_train, n_jobs=-1, cv=10, scoring='r2').mean()
+    def objective(trial):
+        param = {
+            "silent": 1,
+            "objective": "reg:squarederror",
+            "eval_metric": r2_score,
+            "booster": trial.suggest_categorical("booster", ["gbtree"]), #, "gblinear", "dart"]),
+            "lambda": trial.suggest_loguniform("lambda", 1e-8, 1.0e6),
+            "alpha": trial.suggest_loguniform("alpha", 1e-8, 1.0e6),
+        }
+        if param["booster"] == "gbtree" or param["booster"] == "dart":
+            param["max_depth"] = trial.suggest_int("max_depth", 2, 6)
+            param["eta"] = trial.suggest_loguniform("eta", 1e-8, 0.05)
+            param["gamma"] = trial.suggest_loguniform("gamma", 1e-8, 1.0)
+            param["grow_policy"] = trial.suggest_categorical("grow_policy", ["depthwise", "lossguide"])
+        if param["booster"] == "dart":
+            param["sample_type"] = trial.suggest_categorical("sample_type", ["uniform", "weighted"])
+            param["normalize_type"] = trial.suggest_categorical("normalize_type", ["tree", "forest"])
+            param["rate_drop"] = trial.suggest_loguniform("rate_drop", 1e-8, 1.0)
+            param["skip_drop"] = trial.suggest_loguniform("skip_drop", 1e-8, 1.0)
+        
+        model = xgb.XGBRegressor(**param, n_jobs=-1)
+        
+        return cross_val_score(model, X_train, y_train, n_jobs=-1, cv=10, scoring='r2').mean()
+    return objective
 
-# train function
+
 def train(args):
     '''Train fucntion to create a model.
     '''
@@ -99,18 +101,24 @@ def train(args):
         config['feat_cols'], 
         dataset['embed']
         )
+    all_feats = X_train_df.columns.tolist() #store feat names to a var
     y_train = dataset['train'].loc[:,'Sale_Price']
     # run imputation for missing values and save it
     X_train = impute_missing(X_train_df, args.out)
 
     # Create HPO and find best params
     study = optuna.create_study(direction='maximize')
-    study.optimize(objective, n_trials=100)
-    model = xgb.XGBRegressor(**study.best_params, feature_names=X_train_df.columns.tolist())
+    objective = make_objective(X_train, y_train)
+    print("Starting parameter search")
+    study.optimize(objective, n_trials=1)
+    
+    model = xgb.XGBRegressor(**study.best_params, feature_names=all_feats)
     model.fit(X_train, y_train)
     # save best model
     model.save_model(os.path.join(args.out,"xgb_model.json"))
 
+    # print best metrics train and test
+    print(f'Train R^2 Score: {model.score(X_train,y_train):2.5f}')
     pdb.set_trace()
 
 if __name__=='__main__':
